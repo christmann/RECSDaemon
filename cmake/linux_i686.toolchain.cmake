dc4c0d1f1fb4b3cc555f1b451d0d21eb6c169eaf
Micha vor dem Berge <micha.vordemberge@christmann.info>
2022-11-10 15:54:01 +0100

    Add local toolchain cmake file

---
+++ cmake/linux_i686.toolchain.cmake
@@ -0,0 +1,11 @@
+message("setting i686 setting in toolchain file")
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_VERSION 1)
+set(CMAKE_SYSTEM_PROCESSOR "i686")
+
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -m32" CACHE STRING "c++ flags")
+set(CMAKE_C_FLAGS   "${CMAKE_C_FLAGS} -m32" CACHE STRING "c flags")
+set(CMAKE_EXE_LINKER_FLAGS  "${CMAKE_EXE_LINKER_FLAGS} -L/lib32/")
+LIST(APPEND CMAKE_SYSTEM_LIBRARY_PATH
+  /usr/lib/i386-linux-gnu/
+)
