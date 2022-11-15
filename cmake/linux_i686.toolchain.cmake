d0bafaa32e841c272a521a8d9d231e71fe0e2370
Micha vor dem Berge <micha.vordemberge@christmann.info>
2022-11-15 16:06:52 +0100

    Cmake: Use gcc and g++ as compilers

--- cmake/linux_i686.toolchain.cmake
+++ cmake/linux_i686.toolchain.cmake
@@ -4,6 +4,10 @@ set(CMAKE_SYSTEM_VERSION 1)
 set(CMAKE_SYSTEM_PROCESSOR "i686")
 
 
+# which compilers to use for C and C++
+set(CMAKE_C_COMPILER   gcc)
+set(CMAKE_CXX_COMPILER g++)
+
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -m32" CACHE STRING "c++ flags")
 set(CMAKE_C_FLAGS   "${CMAKE_C_FLAGS} -m32" CACHE STRING "c flags")
 set(CMAKE_EXE_LINKER_FLAGS  "${CMAKE_EXE_LINKER_FLAGS} -L/lib32/")
