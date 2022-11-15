d05a94abb37417186715198760239dfeaefaa7de
Micha vor dem Berge <micha.vordemberge@christmann.info>
2022-11-15 15:34:38 +0100

    Try to fix cmake i386 cross-compilation

--- cmake/linux_i686.toolchain.cmake
+++ cmake/linux_i686.toolchain.cmake
@@ -3,9 +3,18 @@ set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_VERSION 1)
 set(CMAKE_SYSTEM_PROCESSOR "i686")
 
+
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -m32" CACHE STRING "c++ flags")
 set(CMAKE_C_FLAGS   "${CMAKE_C_FLAGS} -m32" CACHE STRING "c flags")
 set(CMAKE_EXE_LINKER_FLAGS  "${CMAKE_EXE_LINKER_FLAGS} -L/lib32/")
 LIST(APPEND CMAKE_SYSTEM_LIBRARY_PATH
   /usr/lib/i386-linux-gnu/
 )
+
+# adjust the default behavior of the FIND_XXX() commands:
+# search programs in the host environment
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+
+# search headers and libraries in the target environment
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
