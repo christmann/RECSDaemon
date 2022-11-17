27f05ef058cd39c742d6a07c2fca2c88d0a2336f
Micha vor dem Berge <micha.vordemberge@christmann.info>
2022-11-17 15:32:37 +0100

    uiae

--- cmake/linux_arm.toolchain.cmake
+++ cmake/linux_arm.toolchain.cmake
@@ -20,4 +20,4 @@ include_directories(BEFORE /usr/include/arm-linux-gnueabihf/)
 SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 # for libraries and headers in the target directories
 SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
-SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE BOTH)
+SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
