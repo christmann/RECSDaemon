d5c0ca89f30cb1f870620cf334f253586239298c
mvdberge <mvdberge@users.noreply.github.com>
2022-11-21 11:24:43 +0100

    Fixing cmake files for compilation on modern Linux (#1)
    * Try fixing i386 cmake cross-compilation
    
    * Revert "Try fixing i386 cmake cross-compilation"
    
    This reverts commit 7333e26d1d4f002af99ec5caf91a5cd1d571d68d.
    
    * Add local toolchain cmake file
    
    * Try to fix cmake i386 cross-compilation
    
    * Cmake: Use gcc and g++ as compilers
    
    * Update eclipse project files
    
    * Add build dirs and make files to gitignore
    
    * Include crypto lib in cmake
    
    * Don't set ssl paths explicit, let cmake guess it
    
    * Add cmake arm toolchain file
    
    * Cosmetic fixes of i686 toolchain file
    
    * Set arm processor in cmake toolchain file
    
    * Set arm system lib path
    
    * Don't set arm root path
    
    * try fix arm cmake
    
    * try fix arm cmake
    
    * Add quotes
    
    * Revert "Add quotes"
    
    This reverts commit 41312938527f7b95cddb7910e4d3ae6b5e434a8e.
    
    * Manually include /usr/include
    
    * Look for includes in host dir and cross-build dirs
    
    * Use gcc-linaro-7.5 compiler
    
    * Use default gcc
    
    * Revert "Use default gcc"
    
    This reverts commit 8744a6b649be549e8f2634a74f31669c13ea028c.
    
    * Try arm compiler 11.3
    
    * Revert "Try arm compiler 11.3"
    
    This reverts commit 6af06e2c65a32af38a7bd2d1dd861f6befb17c6b.
    
    * Don't overwrite include dirs
    
    * Try fix include dirs
    
    * Try fix includes
    
    * Use default includes for arm
    
    * Include root path includes
    
    * Don't include /usr/include directly
    
    * Include /usr/include/arm-linux-gnueabihf/ explicit
    
    * uiae
    
    * Set cmake package
    
    * Setting CMAKE_FIND_ROOT_PATH
    
    * Set OPENSSL_ROOT_DIR
    
    * Add / as root path
    
    * Change back to "include_dirs"
    
    * bla debug
    
    * set /usr/include as include path
    
    * Add debug message
    
    * Force /usr/include as CXX FLAG
    
    * Try include with -I
    
    * Add /usr/include/arm-linux-gnueabihf to includes
    
    * Cleanup

---
+++ cmake/linux_arm.toolchain.cmake
@@ -0,0 +1,28 @@
+message("Setting arm setting in toolchain file")
+SET(CMAKE_SYSTEM_NAME Linux)
+SET(CMAKE_SYSTEM_VERSION 1)
+set(CMAKE_SYSTEM_PROCESSOR arm)
+
+# specify the cross compiler
+set(tools /opt/gcc-linaro-7.5)
+set(CMAKE_C_COMPILER ${tools}/bin/arm-linux-gnueabihf-gcc)
+set(CMAKE_CXX_COMPILER ${tools}/bin/arm-linux-gnueabihf-g++)
+
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -marm" CACHE STRING "c++ flags")
+set(CMAKE_C_FLAGS   "${CMAKE_C_FLAGS} -marm" CACHE STRING "c flags")
+set(CMAKE_EXE_LINKER_FLAGS  "${CMAKE_EXE_LINKER_FLAGS} -L/lib/arm-linux-gnueabihf/")
+LIST(APPEND CMAKE_SYSTEM_LIBRARY_PATH
+  /usr/lib/arm-linux-gnueabihf/
+)
+
+set(CMAKE_FIND_ROOT_PATH ${tools};/)
+
+# search for programs in the build host directories
+SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+# for libraries and headers in the target directories
+SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
+
+# Force the toolchain to search includes also in /usr/include and /usr/include/arm-linux-gnueabihf/
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -idirafter /usr/include/ -idirafter /usr/include/arm-linux-gnueabihf/")
